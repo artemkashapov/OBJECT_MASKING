a2f954a84da3f5bfd10f017fc7504ba3190871d5
Artem Kashapov <ar.kashapov@gmail.com>
2021-03-24 14:52:15 +0400

    Добавлен метод сортировки

---
+++ src/main/java/OBJECT_MASKING/functions/UnsortedToSorted.java
@@ -0,0 +1,116 @@
+package OBJECT_MASKING.functions;
+
+public class UnsortedToSorted {
+    private final int[] numVector;
+    private final double[] sumVector;
+    private double[] intervalVector;
+    private int[] priorities;
+
+    public UnsortedToSorted(double[] sumVector, double[] intervalVector) {
+        this.sumVector = new double[sumVector.length];
+        this.numVector = new int[sumVector.length];
+        this.intervalVector = new double[intervalVector.length];
+        for (int i = 0; i < sumVector.length; i++) {
+            this.numVector[i] = i + 1;
+            this.sumVector[i] = sumVector[i];
+        }
+
+        System.arraycopy(intervalVector, 0, this.intervalVector, 0, intervalVector.length);
+    }
+
+    public UnsortedToSorted(double[] sumVector) {
+        this.sumVector = new double[sumVector.length];
+        this.numVector = new int[sumVector.length];
+        for (int i = 0; i < sumVector.length; i++) {
+            this.numVector[i] = i + 1;
+            this.sumVector[i] = sumVector[i];
+        }
+    }
+
+    public void bubbleSort() {
+        boolean sorted = false;
+        int tempInd;
+        double temp;
+        while (!sorted) {
+            sorted = true;
+            for (int i = 0; i < sumVector.length - 1; i++) {
+                if (sumVector[i] > sumVector[i + 1]) {
+                    temp = sumVector[i];
+                    tempInd = numVector[i];
+                    sumVector[i] = sumVector[i + 1];
+                    numVector[i] = numVector[i + 1];
+                    numVector[i + 1] = tempInd;
+                    sumVector[i + 1] = temp;
+                    sorted = false;
+                }
+            }
+        }
+
+        double[] tempInt1 = new double[sumVector.length];
+        int[] tempInt2 = new int[numVector.length];
+        System.arraycopy(sumVector, 0, tempInt1, 0, sumVector.length);
+        System.arraycopy(numVector, 0, tempInt2, 0, numVector.length);
+        for (int i = 0; i < tempInt1.length; i++) {
+            sumVector[i] = tempInt1[tempInt1.length - (i + 1)];
+            numVector[i] = tempInt2[tempInt2.length - (i + 1)];
+        }
+    }
+
+    public void intervalSort() {
+        boolean sorted = false;
+        int tempInd;
+        double temp;
+        while (!sorted) {
+            sorted = true;
+            for (int i = 0; i < intervalVector.length - 1; i++) {
+                if (intervalVector[i] > intervalVector[i + 1]) {
+                    temp = intervalVector[i];
+                    intervalVector[i] = intervalVector[i + 1];
+                    intervalVector[i + 1] = temp;
+                    sorted = false;
+                }
+            }
+        }
+
+        double[] tempInt = new double[intervalVector.length];
+        System.arraycopy(intervalVector, 0, tempInt, 0, intervalVector.length);
+        for (int i = 0; i < tempInt.length; i++) {
+            intervalVector[i] = tempInt[tempInt.length - (i + 1)];
+        }
+    }
+
+    public void fullSort() {
+        bubbleSort();
+        intervalSort();
+        int o = 1;
+        int k = 0;
+        int[] priorityVector = new int[sumVector.length];
+        while (o <= intervalVector.length / 2) {
+            for (int i = 0; i < sumVector.length; i++) {
+                if (sumVector[i] <= intervalVector[2 * o - 2] && sumVector[i] >= intervalVector[2 * o - 1]) {
+                    priorityVector[i] = o;
+                }
+            }
+            o++;
+        }
+        this.priorities = new int[priorityVector.length];
+        System.arraycopy(priorityVector, 0, this.priorities, 0, priorityVector.length);
+    }
+
+
+    public double[] getSumVector() {
+        return sumVector;
+    }
+
+    public double[] getIntervalVector() {
+        return intervalVector;
+    }
+
+    public int[] getPriorities() {
+        return priorities;
+    }
+
+    public int[] getNumVector() {
+        return numVector;
+    }
+}
---
+++ src/test/java/OBJECT_MASKING/functions/UnsortedToSortedTest.java
@@ -0,0 +1,87 @@
+package OBJECT_MASKING.functions;
+
+import org.testng.annotations.Test;
+
+import java.util.Arrays;
+
+public class UnsortedToSortedTest {
+
+    private final double[] s1 = {1, 4, 2, 5, 7};
+    private final double[] s2 = {1, 7, 88, 5, 7, 1, 1, 1};
+    private final double[] s3 = {6, 4, 1, 59, 7, 12};
+
+    private final double[] Ko = {2.6, 2.6, 2.8, 2.7, 3.1, 1.3, 3.3, 3.4, 0.6, 0.9};
+    private final double[] intervals = {3.1, 4.0, 2.1, 3.0, 1.1, 2.0, 0, 1.0};
+
+
+    private final double[] i1 = {1, 3, 6, 7, 12, 59};
+
+    private UnsortedToSorted getElem(double[] s) {
+        return new UnsortedToSorted(s);
+    }
+
+    private UnsortedToSorted getElems1(double[] s, double[] ints) {
+        return new UnsortedToSorted(s, ints);
+    }
+
+    @Test
+    public void testBubbleSort() {
+        UnsortedToSorted t1 = getElem(s1);
+        UnsortedToSorted t2 = getElem(s2);
+        UnsortedToSorted t3 = getElem(s3);
+        t1.bubbleSort();
+        t2.bubbleSort();
+        t3.bubbleSort();
+
+        System.out.println(Arrays.toString(t1.getNumVector()));
+        System.out.println(Arrays.toString(t1.getSumVector()));
+
+        System.out.println(Arrays.toString(t2.getNumVector()));
+        System.out.println(Arrays.toString(t2.getSumVector()));
+
+        System.out.println(Arrays.toString(t3.getNumVector()));
+        System.out.println(Arrays.toString(t3.getSumVector()));
+
+    }
+
+    @Test
+    public void testGetSumVector() {
+        UnsortedToSorted t1 = getElem(s1);
+        UnsortedToSorted t2 = getElem(s2);
+        UnsortedToSorted t3 = getElem(s3);
+
+        System.out.println(Arrays.toString(t1.getNumVector()));
+        System.out.println(Arrays.toString(t1.getSumVector()));
+
+        System.out.println(Arrays.toString(t2.getNumVector()));
+        System.out.println(Arrays.toString(t2.getSumVector()));
+
+        System.out.println(Arrays.toString(t3.getNumVector()));
+        System.out.println(Arrays.toString(t3.getSumVector()));
+
+    }
+
+    @Test
+    public void testIntervalSort() {
+        UnsortedToSorted ints = getElems1(s1, i1);
+
+        System.out.println(Arrays.toString(ints.getIntervalVector()));
+
+        ints.intervalSort();
+        System.out.println(Arrays.toString(ints.getIntervalVector()));
+    }
+
+    @Test
+    public void testFullTest() {
+        UnsortedToSorted ex1 = getElems1(Ko, intervals);
+        //System.out.println(Arrays.toString(ex1.getNumVector()));
+        //System.out.println(Arrays.toString(ex1.getSumVector()));
+        //System.out.println(Arrays.toString(ex1.getIntervalVector()));
+
+        ex1.fullSort();
+        System.out.println(Arrays.toString(ex1.getNumVector()));
+        System.out.println(Arrays.toString(ex1.getPriorities()));
+        System.out.println(Arrays.toString(ex1.getSumVector()));
+
+    }
+}
